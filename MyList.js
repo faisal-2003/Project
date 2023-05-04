4f184d59866471a2d6c03c0942918b2f2304c316
faisal-2003 <123529663+faisal-2003@users.noreply.github.com>
2023-05-04 14:54:31 +0530

    Add files via upload

---
+++ MyList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from 'axios'
+class MyList extends React.Component{
+    state = {
+        posts : []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response =>{
+            const posts = response.data;
+            this.setState({posts});
+        })
+    }
+    
+    render(){
+        return(
+            <div>
+                <ul>
+                    {this.state.posts.map(Post =>
+                        <li key = {Post.id}><b>Title</b> : {Post.title} <br/>
+                                              Body : {Post.body}
+                                              </li>)}
+                </ul>
+            </div>
+        )
+    }
+}
+export default MyList;
